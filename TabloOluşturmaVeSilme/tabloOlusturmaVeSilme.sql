fb11efb672d5e9e1bbd298ee63b0fe9887cbe5dc
buraktitiz <titizburak10@gmail.com>
2023-05-09 13:51:19 +0300

    SQL Kodları içerisinde düzenlemeler yapıldı.

--- TabloOluşturmaVeSilme/tabloOlusturmaVeSilme.sql
+++ TabloOluşturmaVeSilme/tabloOlusturmaVeSilme.sql
@@ -7,43 +7,43 @@
 -- )
 
 -- Aşağıdaki kod author adında yeni bir tablo oluşturur.
--- CREATE TABLE author(
--- 	id SERIAL PRIMARY KEY,
--- 	first_name VARCHAR(50) NOT NULL,
--- 	last_name VARCHAR(50) NOT NULL,
--- 	email VARCHAR(100),
--- 	birthday DATE
--- )
+CREATE TABLE author(
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100),
+	birthday DATE
+)
 
--- SELECT * FROM author
+SELECT * FROM author
 
 -- Tabloya yeni veriler ekleyelim
--- INSERT INTO author (first_name, last_name, email, birthday)
--- VALUES
--- 	('Sabahattin' , 'Ali' , 'sabahattin@ali.com', '1953-07-06'),
--- 	('William' , 'Shakespeare' , 'william@shakespeare.com', '1942-09-18'),
--- 	('ReşatNuri' , 'Güntekin' , 'resatnuri@guntekin.com', '1936-11-28'),
--- 	('Adam' , 'Fawer' , 'adam@fawer.com', '1943-01-29'),
--- 	('Aleksandr' , 'Dumas' , 'aleksandr@dumas.com', '1918-12-23');
+INSERT INTO author (first_name, last_name, email, birthday)
+VALUES
+	('Sabahattin' , 'Ali' , 'sabahattin@ali.com', '1953-07-06'),
+	('William' , 'Shakespeare' , 'william@shakespeare.com', '1942-09-18'),
+	('ReşatNuri' , 'Güntekin' , 'resatnuri@guntekin.com', '1936-11-28'),
+	('Adam' , 'Fawer' , 'adam@fawer.com', '1943-01-29'),
+	('Aleksandr' , 'Dumas' , 'aleksandr@dumas.com', '1918-12-23');
 	
 --CREATE TABLE author2 (LIKE author) 
 -- Yukarıda oluşturduğumuz author2 tablosunu author tablosundan aldık.
 -- Yalnız author2 tablomuzun içinde herhangi bir veri yok. Çünkü veriyle beraber değil, tablonun yapısını aldık.
--- SELECT * FROM author2; -- yazarak görebiliriz
+SELECT * FROM author2; -- yazarak görebiliriz
 
 -- Aşağıda author tablomuzdan author2 tablomuza ismi Aleksandr olan veriyi kopyalayalım.
--- INSERT INTO author2
--- SELECT * FROM author
--- WHERE first_name = 'Aleksandr';
+INSERT INTO author2
+SELECT * FROM author
+WHERE first_name = 'Aleksandr';
 
--- SELECT * FROM author2; -- yazıp görebiliriz
+SELECT * FROM author2; -- yazıp görebiliriz
 
 -- Bu defa author3 adında yeni bir tablo oluşturalım ve oluşturduğumuz tabloya author tablosundaki tüm  verileriyle beraber kopyalayalım
--- CREATE TABLE author3 AS
--- SELECT * FROM author;
+CREATE TABLE author3 AS
+SELECT * FROM author;
 
 -- SELECT * FROM author3 -- yazarak görebiliriz
 
 -- Tabloları silmek için de aşağıdaki kodu kullanabiliriz
--- DROP TABLE author3; --author3 tablosunu sildik
--- DROP TABLE author2; --author2 tablosunu sildik
+DROP TABLE author3; --author3 tablosunu sildik
+DROP TABLE author2; --author2 tablosunu sildik
--- TabloOluşturmaVeSilme/verileriGuncellemek.sql
+++ TabloOluşturmaVeSilme/verileriGuncellemek.sql
@@ -5,47 +5,47 @@
 -- WHERE condition;
 
 -- Aşağıdaki kodumuzda first_name'i V ile başlayan veriyi first_name'i XXXX last_name'i YYYY olacak şekilde güncelledik.
--- UPDATE author
--- SET first_name='XXXX',
--- 	last_name='YYYY'
--- WHERE first_name LIKE 'V%';
+UPDATE author
+SET first_name='XXXX',
+	last_name='YYYY'
+WHERE first_name LIKE 'V%';
 
--- SELECT * FROM author;
+SELECT * FROM author;
 
 -- Aşağıdaki kodumuzda id'si 10 olan veriyi first_name'i Emrah Safa, last_name'i Gürkan olarak değiştirdik. 
 -- Değiştirdiğimiz veri en sonda yer alacaktır. 
 -- Bundan sonra da güncellenecek veriler en sonda yer alacak şekilde sıralancaktır.
--- UPDATE author
--- SET first_name='Emrah Safa',
--- 	last_name='Gürkan',
--- 	email='emrah@gurkan.com',
--- 	birthday='1980-01-01'
--- WHERE id=10;
+UPDATE author
+SET first_name='Emrah Safa',
+	last_name='Gürkan',
+	email='emrah@gurkan.com',
+	birthday='1980-01-01'
+WHERE id=10;
 
--- SELECT * FROM author;
+SELECT * FROM author;
 
 -- Aşağıdaki kodumuzda first_name'i Rivi olan verinin last_name'ini Yeni Soyad olarak değiştirip RETURNING anahtar kelimesiyle tüm satırın return edilmesini sağlayalım.
--- UPDATE author
--- SET last_name = 'Yeni Soyad'
--- WHERE first_name ='Rivi'
--- RETURNING *;
+UPDATE author
+SET last_name = 'Yeni Soyad'
+WHERE first_name ='Rivi'
+RETURNING *;
 
--- SELECT * FROM author;
+SELECT * FROM author;
 
 
 -- *VERİLERİN SİLİNMESİ*
--- DELETE <table_name>
--- WHERE condition;
+DELETE <table_name>
+WHERE condition;
 
 -- id'si 6 olan veriyi sildik.
---DELETE FROM author
---WHERE id=6;
+DELETE FROM author
+WHERE id=6;
 
---SELECT * FROM author;
+SELECT * FROM author;
 
 -- id'si 11'den yüksek olan tüm verileri silip RETURNING anahtar kelimesiyle silinen tüm satırlar geri dönmüş olacaktır. 
--- DELETE FROM author
--- WHERE id>11
--- RETURNING *;
+DELETE FROM author
+WHERE id>11
+RETURNING *;
 
--- SELECT * FROM author; -- bu kodumuz da güncel şekilde, kalan tüm satırları görmemizi sağlayacaktır.
+SELECT * FROM author; -- bu kodumuz da güncel şekilde, kalan tüm satırları görmemizi sağlayacaktır.
