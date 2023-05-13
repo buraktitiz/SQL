25d46fdfdb76d80ea512daf6ced616fe61881a38
buraktitiz <titizburak10@gmail.com>
2023-05-13 13:58:11 +0300

    SQL project to work with Post SQL.

--- HomeWork-12/homework-12.sql
+++ HomeWork-12/homework-12.sql
@@ -25,7 +25,13 @@ AND replacement_cost=
 
 
 -- 4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
-SELECT customer_id, COUNT(customer_id) FROM payment
-GROUP BY customer_id 
-ORDER BY COUNT(customer_id) DESC;
+-- SELECT customer_id, COUNT(customer_id) FROM payment
+-- GROUP BY customer_id 
+-- ORDER BY COUNT(customer_id) DESC;
 
+
+SELECT SUM(amount), customer.first_name, customer.last_name FROM payment
+JOIN customer ON payment.customer_id = customer.customer_id
+GROUP BY payment.customer_id, customer.first_name, customer.last_name
+ORDER BY SUM(amount) DESC;
+LIMIT 1;
